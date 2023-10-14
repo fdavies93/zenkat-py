8e5752b6296abeaeda26a5e276776954b88333c5
Frank Davies <fdavies93@gmail.com>
2023-10-14 20:31:18 +0800

    Make outline use filters over list items

--- src/zenkat/zenkat.py
+++ src/zenkat/zenkat.py
@@ -57,6 +57,7 @@ class Page:
     headings: list = field(default_factory=list)
     heading_tree: list = field(default_factory=list)
     lists: list = field(default_factory=list)
+    lists_tree: list = field(default_factory=list)
 
 @dataclass
 class Heading:
@@ -152,12 +153,18 @@ def get_heading_tree(title: str, document: str):
     
     return root
 
-def node_tree_dft(root, child_property: str, do_fn: Callable):
+def node_tree_dft(root, child_property: str, do_fn: Callable[[object],bool]):
+    '''
+    Perform a depth first traversal of a node tree of objects and call do_fn at each node. If do_fn returns False, don't continue to navigate that branch of the tree.
+    '''
     # perform a Depth First Traversal of a node tree and do something at each node
-    do_fn(root)
+    outcome = do_fn(root)
+    if not outcome:
+        return False
     children = root.__dict__[child_property]
     for child in children:
         node_tree_dft(child, child_property, do_fn)
+    return True
     
 
 def get_lists(document: str, todo_map: dict):
--- src/zenkat/zk.py
+++ src/zenkat/zk.py
@@ -206,7 +206,16 @@ def cmd_outline(args, console: Console, config: dict):
 
     root_tag, body_tag, spacer_tag, spacer, spacer_end = [outline[v] for v in ("root_tag","body_tag","spacer_tag","spacer","spacer_end")]
 
+    filter_strs = []
+    if args.filter != None:
+        filter_strs = args.filter
+
+    filters = [zenkat.parse_filter(f, zenkat.Heading) for f in filter_strs]
+    
     def print_node(node: zenkat.Heading):
+        for f in filters:
+            if not f(node): return False
+        
         output = ""
         bt = body_tag
         if node.depth == 0: 
@@ -214,6 +223,8 @@ def cmd_outline(args, console: Console, config: dict):
         output += spacer_tag[0] + (node.depth * spacer) + spacer_end + spacer_tag[1] + bt[0] + node.text + bt[1]
 
         console.print(output)
+        
+        return True
 
     for page in index.pages:
         zenkat.node_tree_dft(page.heading_tree, "children", print_node)
