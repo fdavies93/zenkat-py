41311dff45fcba22881e5a7ef3495c6e4a2ca12c
Frank Davies <fdavies93@gmail.com>
2023-10-25 19:32:33 +0800

    Format strings are parsed to a node tree

--- src/zenkat/format.py
+++ src/zenkat/format.py
@@ -2,6 +2,10 @@ from zenkat import fields
 from dataclasses import dataclass
 import re
 from typing import Union
+from copy import deepcopy
+from rich.markdown import Markdown
+from rich.console import Console
+from zenkat.utils import node_tree_dft
 
 # figure out process for rendering ALL segments of a string from a format string via rich and later assembling them
 # USE OWN TEMPLATING LANGUAGE / JINJA LIKE SYNTAX
@@ -64,10 +68,61 @@ def lex_styles(f_str):
 
     return tokens
 
+def parse_styles(tokens: list[str], styles = set()) -> tuple[Block, list[str]]:
+
+    # PEG
+    # block := { tag+ }? ~ (block | string)* ~ { end tag+ }?
+
+    cur_styles = styles
+    pattern = re.compile("{:(.*?):}")
+    children: list[Union[Block, str]] = []
+    remaining = deepcopy(tokens)
+    
+
+    next = remaining[0]
+    next_match = pattern.match(next)
+
+    if next_match is not None:
+        # i.e. it's a tag
+        style_names = next_match.group(1).split()
+        cur_styles = styles.union(set(style_names))         
+        remaining = remaining[1:]
+
+    while len(remaining) > 0:
+        # break if you discover an end tag
+        next = remaining[0]
+        next_match = pattern.match(next)
+        
+        if next_match is not None:
+            # i.e. it's a tag        
+            style_names = next_match.group(1).split()
+            # allows end tag to have syntactic sugar names
+            if style_names[0] == "end":
+                remaining = remaining[1:]
+                break
+            # include the starting tag so that the child can set its styles from it
+            child = parse_styles(remaining, set(style_names).union(styles))
+            children.append(child[0])
+            remaining = child[1]
+            continue
+        
+        # i.e. it's just a string
+        children.append(next)
+        remaining = remaining[1:]            
+
+    return (Block(cur_styles, children), remaining)
+
+def render_to_console_str(root: Block, console: Console, short_names: dict = {}) -> str:
+    output_str = ""
+    return output_str
+    
+
 def format(f_str, obj):
     replaced = replace_from(obj, f_str)
     lexed = lex_styles(replaced)
-    return lexed
+    print(lexed)
+    parsed = parse_styles(lexed)
+    return parsed
 
 
 def format_list(objs : list, f_str : str):
