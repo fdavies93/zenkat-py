09edf50e698e3b29e7726f210b99fdcb111d1169
Frank Davies <fdavies93@gmail.com>
2023-10-30 20:26:18 +0800

    Groups in --group are now ordered

--- src/zenkat/group.py
+++ src/zenkat/group.py
@@ -1,10 +1,16 @@
 from zenkat.fields import get_field_fn
+from collections import OrderedDict
 
-def group(objects: list, field_name: str):
-    obj_dict = dict()
+def group(objects: list, field_name: str, reverse=False):
+    obj_dict: OrderedDict = OrderedDict()
     for obj in objects:
-        field_val = str(get_field_fn(obj, field_name))
+        field_val = get_field_fn(obj, field_name)
         if field_val not in obj_dict: obj_dict[field_val] = []
         obj_dict[field_val].append(obj)
+
+    ordered_dict = OrderedDict()
+
+    for k in sorted(obj_dict.keys(), reverse=reverse):
+        ordered_dict[str(k)] = obj_dict[k]
      
-    return obj_dict
+    return ordered_dict
